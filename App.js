a2fbaf16a1cdcf08ea05c0e37ff8a34076231b79
Rupika01 <rg.rupi19@gmail.com>
2024-09-08 20:55:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+function Calc() {
+	const [Input, setInput] = useState("red");
+	const [Calc, setCalc] = useState("red");
+
+	const input = ['/', '*', '-', '+', '.'];
+}
+
